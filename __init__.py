0751fbb60bc97e6de7f0e39d3d5441b73c5f0368
DHY <dooheeucb@gmail.com>
2025-11-17 08:22:49 -0800

    user_data output

--- __init__.py
+++ __init__.py
@@ -1,9 +1,22 @@
 """
-Data loading utilities for experiments.
+LLM Architecture Experimentation Framework
 """
-from .dataset import LanguageModelDataset, get_dataloaders
+from .config import ExperimentConfig, ExperimentResults
+from .model import CustomGPT2Model
+from .trainer import ArchitectureExperiment
+from .metrics import MetricsCalculator, MetricsTracker
+from .analysis import compare_experiments, plot_training_curves, generate_summary_report
+
+__version__ = "0.1.0"
 
 __all__ = [
-    'LanguageModelDataset',
-    'get_dataloaders',
+    'ExperimentConfig',
+    'ExperimentResults',
+    'CustomGPT2Model',
+    'ArchitectureExperiment',
+    'MetricsCalculator',
+    'MetricsTracker',
+    'compare_experiments',
+    'plot_training_curves',
+    'generate_summary_report',
 ]
